490b66f729fd923dc61a604f72cabb2467581031
Gapper1000 <143434072+Gapper1000@users.noreply.github.com>
2023-12-05 12:43:15 -0500

    created simple excel file reader

--- app/fileReader.py
+++ app/fileReader.py
@@ -1,22 +1,7 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+import openpyxl
 
-# Set up credentials
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+filePath = "/Users/gmp/Documents/GitHub/chore-tracker/Chore Tracker.xlsx"
 
-# Specify the path to your JSON key file (replace with the actual path)
-json_key_path = "/Users/gmp/Documents/GitHub/chore-tracker/config/freestyle-project-407217-3968e1b66986.json"
-creds = ServiceAccountCredentials.from_json_keyfile_name(json_key_path, scope)
-
-# Authenticate with Google Sheets API
-client = gspread.authorize(creds)
-
-# Open the Google Sheet by title
-sheet = client.open_by_key("1rta3xYgJb-2iWAAw0lXJ1LiB0Z0OOLTyRZQ7HNSnB14").sheet1
-
-# Get all values from the sheet
-all_values = sheet.get_all_values()
-
-# Print the values
-for row in all_values:
-    print(row)
+df = pd.read_excel(filePath)
+print(df)
