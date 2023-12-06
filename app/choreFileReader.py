b20db951723cdaf74d41b653f647a2c9942a8847
Gapper1000 <143434072+Gapper1000@users.noreply.github.com>
2023-12-06 16:49:17 -0500

    Fixed file upload for tasks

--- app/choreFileReader.py
+++ app/choreFileReader.py
@@ -4,17 +4,17 @@ from openpyxl import load_workbook
 
 filePath = "/Users/gmp/Documents/GitHub/chore-tracker/Chore Tracker.xlsx"
 selected_columns = ['Assignee', 'Task', 'Due Date', 'Status']
-chore_df = pd.read_excel(filePath, usecols=selected_columns)
 
 
-def readFile(chore_df):
-        chore_df['Assignee'] = chore_df['Assignee'].astype(object)
-        chore_df['Task'] = chore_df['Task'].astype(str)
-        chore_df['Due Date'] = pd.to_datetime(chore_df['Due Date'], errors='coerce')
-        for index, row in chore_df.iterrows():
-            if pd.isnull(row['Status']):
-                chore_df.at[index, 'Status'] = 0
-        chore_df['Status'] = chore_df['Status'].astype(bool)
+def readFile(filePath):
+        chore_df = pd.read_excel(filePath, usecols=selected_columns)
+        # chore_df['Assignee'] = chore_df['Assignee'].astype(object)
+        # chore_df['Task'] = chore_df['Task'].astype(str)
+        # chore_df['Due Date'] = pd.to_datetime(chore_df['Due Date'], errors='coerce')
+        # for index, row in chore_df.iterrows():
+        #     if pd.isnull(row['Status']):
+        #         chore_df.at[index, 'Status'] = 0
+        # chore_df['Status'] = chore_df['Status'].astype(bool)
         return chore_df
 
 def editStatus(chore_df, task_name=str, new_status=bool):
--- web_app/routes/tasks_routes.py
+++ web_app/routes/tasks_routes.py
@@ -1,35 +1,58 @@
 from flask import Blueprint, request, render_template, redirect, flash
 from app.choreFileReader import readFile
+import os
+from werkzeug.utils import secure_filename
 
-filePath = "/Users/gmp/Documents/GitHub/chore-tracker/Chore Tracker.xlsx" 
+filePath = "/Users/gmp/Documents/GitHub/chore-tracker/Chore Tracker.xlsx"
 
 tasks_routes = Blueprint("tasks_routes", __name__)
 
-@tasks_routes.route("/tasks/table")
+@tasks_routes.route("/tasks/table", methods=["GET", "POST"])
 def tasks_table():
-    print("tasks table")
+    print("tasks/table")
 
-    try:
-        chore_df = readFile(filePath)
-        latest_chore = chore_df.iloc[0] 
-        latest_due_date = latest_chore["Due Date"]
-        latest_status = "Done" if latest_chore["Status"] else "Pending"
-        
-        flash("Fetched Latest Chore Data", "success")
-        return render_template("table.html", 
-                latest_due_date=latest_due_date,
-                latest_status=latest_status,
-                chore_df = chore_df
-        )
-    except Exception as err:
-        print('OOPS', err)
+    if request.method == "POST":
+        # Check if the POST request has the file part
+        if "file" not in request.files:
+            flash("No file part", "danger")
+            return redirect(request.url)
 
-        flash("Chore data error, please try again!", "danger")
-        return redirect("/")
+        file = request.files["file"]
+
+        # If the user does not select a file, the browser will submit an empty file
+        if file.filename == "":
+            flash("No selected file", "danger")
+            return redirect(request.url)
+
+        if file:
+            # Save the file to a secure location
+            filename = secure_filename(file.filename)
+            file.save(os.path.join("uploads", filename))
+
+            try:
+                chore_df = readFile(os.path.join("uploads", filename))
+                table_html = chore_df.to_html(classes='table table-bordered table-hover', index=False)
+                flash("Fetched Latest Chore Data", "success")
+                return render_template("table.html", table_html=table_html)
+            except Exception as err:
+                print('OOPS', err)
+                flash("Chore data error, please try again!", "danger")
+                return redirect("/")
+                
+    else:
+        try:
+            chore_df = readFile(filePath)
+            print(chore_df)
+            table_html = chore_df.to_html(classes='table table-bordered table-hover', index=False)
+
+            flash("Fetched Latest Chore Data", "success")
+            return render_template("table.html", table_html=table_html)
+        except Exception as err:
+            print('OOPS', err)
+            flash("Chore data error, please try again!", "danger")
+            return redirect("/")
 
 @tasks_routes.route("/tasks/form")
 def tasks_form():
     print("tasks form")
     return render_template("tasks.html")
-
-#chore page here
