1eed5e936277785678d84c036bcf260b26448dfa
Gapper1000 <143434072+Gapper1000@users.noreply.github.com>
2023-12-11 09:26:43 -0500

    Task progress (#3)
    * implemented saving of updated tasks
    
    * Implemented file downloading

--- web_app/routes/tasks_routes.py
+++ web_app/routes/tasks_routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, request, render_template, redirect, flash, session
-from app.choreFileReader import readFile
+from flask import Blueprint, request, render_template, redirect, flash, session, send_from_directory
+from app.choreFileReader import readFile, update_workbook
 import os
 from werkzeug.utils import secure_filename
 
@@ -14,12 +14,16 @@ def tasks_table():
             # Process the uploaded file
             filename = secure_filename(file.filename)
             file_path = os.path.join("uploads", filename)
-            file.save(file_path)
-            session['file_path'] = file_path
+            full_file_path = os.path.abspath(file_path)  # Get the absolute file path
+            file.save(full_file_path)
+            session['file_path'] = full_file_path  # Store the absolute file path in the session
             session['file_name'] = filename
         else:
             # If no file was uploaded in this POST request
             flash("No file uploaded. Please upload a file.", "danger")
+
+    # The rest of your code...
+
 
     if 'file_path' in session:
         # If there is a file in the session, try to read and display it
@@ -55,3 +59,31 @@ def tasks_form():
     else:
         flash("No file selected, please upload a file first", "danger")
         return redirect("/tasks/table")
+
+@tasks_routes.route("/tasks/update", methods=["POST"])
+def update_tasks():
+    checked_tasks = request.form.getlist('tasks')
+
+    try:
+        update_workbook(session['file_path'], checked_tasks)
+        flash("Tasks updated successfully", "success")
+    except Exception as e:
+        flash(f"An error occurred while updating tasks: {e}", "danger")
+
+    return redirect('/tasks/table')
+
+@tasks_routes.route("/download-file")
+def download_file():
+    print("download file")
+    file_path = session.get('file_path')
+    print(file_path)
+    if file_path:
+        # Extract directory and filename from the file_path
+        directory = os.path.dirname(file_path)
+        print(directory)
+        filename = os.path.basename(file_path)
+        print(filename)
+        return send_from_directory(directory, filename, as_attachment=True)
+    else:
+        flash("No file available for download.", "danger")
+        return redirect('/tasks/table')
